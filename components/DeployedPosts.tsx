51f1bf7e9a8a9329eb33209e2e19e16cfed22e0d
Ruben Dinis <ruben.sousa.dinis@gmail.com>
2025-07-05 17:11:49 +0200

    Fix lint

--- components/DeployedPosts.tsx
+++ components/DeployedPosts.tsx
@@ -3,7 +3,7 @@
 import React, { useState } from "react";
 import { useDeployedPostsData } from "@/hooks/useDeployedPosts";
 import { useOriginUrlData } from "@/hooks/useOriginUrl";
-import { Card, CardHeader, CardTitle, CardContent } from "@/components/ui/card";
+import { Card, CardContent } from "@/components/ui/card";
 import { Badge } from "@/components/ui/badge";
 import { ExternalLink, FileText, Copy, Calendar, Hash } from "lucide-react";
 import { cn } from "@/lib/utils";
